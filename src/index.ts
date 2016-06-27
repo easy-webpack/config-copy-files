4fbf2ca9e68e211447145c2d3b7ebb3fa882f986
Dwayne Charrington <dwaynecharrington@gmail.com>
2016-06-27 11:49:35 +1000

    feat(*): added in plugin for copying stuff

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import {WebpackConfig, get} from '@easy-webpack/core';
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+/**
+ * For copying files and folders see here for options: https://github.com/kevlened/copy-webpack-plugin
+ *
+ * Basic usage:
+ *
+ * require('@easy-webpack/config-generate-index-html')
+ * {patterns: [
+ *   {from: 'src/images', to: 'images'}
+ * ]})
+ *
+ */
+export = function copyFiles({patterns: [], options = {}} = {}) {
+  return function copyFiles(this: WebpackConfig): WebpackConfig {
+    const config = {
+      plugins: [
+        new CopyWebpackPlugin(patterns, options)
+      ].concat(get(this, 'plugins', []))
+    }
+  }
+}
